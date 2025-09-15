c427874bb01e1de6202b64e5414335d57202f845
Keyes Anderson <keyes.anderson@gmail.com>
2025-09-14 20:17:55 -0500

    remove conflict, remove link to weather

--- CourtSchedulerFront/court-scheduler/Services/PlayerServices.cs
+++ CourtSchedulerFront/court-scheduler/Services/PlayerServices.cs
@@ -3,16 +3,12 @@
     public class PlayerServices
     {
         public static async Task<IList<Player>> GetPlayers() {
-<<<<<<< Updated upstream
-            return await API.Get<IList<Player>>(new Uri(""))
-=======
             //TODO dns stuff
             return await API.Get<IList<Player>>(new Uri("localhost:3030/players"))
         }
 
         public static async Task<IList<Player>> GetPlayers(int playerId) {
             return await API.Get<IList<Player>>(new Uri($"localhost:3030/players/{playerId}"))
->>>>>>> Stashed changes
         }
     }
 }
