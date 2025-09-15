11fd9a66a5985b79e9259269295e62375bfdf49e
Keyes Anderson <keyes.anderson@gmail.com>
2025-09-14 20:16:46 -0500

    playerservice gets, remove weather page

--- CourtSchedulerFront/court-scheduler/Services/PlayerServices.cs
+++ CourtSchedulerFront/court-scheduler/Services/PlayerServices.cs
@@ -3,7 +3,16 @@
     public class PlayerServices
     {
         public static async Task<IList<Player>> GetPlayers() {
+<<<<<<< Updated upstream
             return await API.Get<IList<Player>>(new Uri(""))
+=======
+            //TODO dns stuff
+            return await API.Get<IList<Player>>(new Uri("localhost:3030/players"))
+        }
+
+        public static async Task<IList<Player>> GetPlayers(int playerId) {
+            return await API.Get<IList<Player>>(new Uri($"localhost:3030/players/{playerId}"))
+>>>>>>> Stashed changes
         }
     }
 }
